4de42b2eb908e1b7c5989e96b0f1b6c8832cc8b3
Samantha Dorrell <samantha.dorrell@yahoo.co.uk>
2022-10-05 14:19:18 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,37 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { styled } from 'nativewind';
+import Landing from './screens/Landing';
+
+const StyledView = styled(View)
+const StyledText = styled(Text)
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import {store} from './redux/store';
-import {Provider} from 'react-redux';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+import Login from "./screens/Login";
+import Register from './screens/Register';
+import Dashboard from './screens/Dashboard';
+import Profile from "./screens/Profile";
 
 const Tab = createMaterialTopTabNavigator();
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
+const App: React.FC = () => {
 
-function SettingsScreen() {
+ 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-export default function App() {
-  return (
+    <>
     <Provider store={store}>
-      <NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <Landing />
+      {/* <NavigationContainer> */}
+        {/* <Tab.Navigator> */}
+          {/* <Tab.Screen name="Landing" component={Landing} /> */}
+          {/* <Tab.Screen name="SignIn" component={Login} />
+          <Tab.Screen name="SignUp" component={Register} />
+        </Tab.Navigator> */}
+      <StatusBar style="dark" />
+      
+      
+      
+      {/* 
         <Tab.Navigator>
           <Tab.Screen name="Home" component={HomeScreen} />
           <Tab.Screen name="Settings" component={SettingsScreen} />
         </Tab.Navigator>
-      </NavigationContainer>
+      </NavigationContainer> */}
+      {/* </NavigationContainer> */}
+    </SafeAreaView>
     </Provider>
+  </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+})
+
+export default App;

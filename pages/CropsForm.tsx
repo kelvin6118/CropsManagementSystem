0d222cf9b5bb3c523e8c41368ca783e70e014641
Yan Wong <kelvinwong26546@gmail.com>
2022-10-07 20:11:27 +0800

    added select list

--- pages/CropsForm.tsx
+++ pages/CropsForm.tsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import SelectList from 'react-native-dropdown-select-list'
 
-const CropsForm = (navigation) => {
+type Props ={
+  navigation: unknown;
+  crops: object[];
+}
+
+const CropsForm: React.FC<Props> = (navigation) => {
+  const [crop, setCrop] = useState<string>();
+  const crops = [
+    {key:"1", value:"Apple"},
+    {key:"2", value:"Banana"},
+    {key:"3", value:"Orange"}
+  ];
   return (
     <View>
-      <Text>CropsForm</Text>
+      <Text>Add New Crop</Text>
+      <View>
+      <SelectList setSelected={setCrop} data={crops} onSelect={() => alert(crop)} />
+      </View>
     </View>
   )
 }

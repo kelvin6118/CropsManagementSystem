d3e5b4a82a7fb108c22fd6153d82de68d5962ae1
Yan Wong <kelvinwong26546@gmail.com>
2022-10-07 17:51:24 +0800

    added Header and css positioning

--- pages/DashBoardHome.tsx
+++ pages/DashBoardHome.tsx
@@ -1,14 +1,20 @@
-import { View, Button } from 'react-native'
+import { View, Button, Text } from 'react-native'
 import React from 'react'
+import DashBoardHeader from '../components/DashBoardHeader'
+import DashBoardTracker from '../components/DashBoardTracker'
 
 
 const DashBoardHome = (props) => {
   return (
-    <View>
-      <Button
-        title="Add Crops"
-        onPress={() => props.navigation.navigate('Form')}
-      />
+    <View className='p-5 space-y-5'>
+      <DashBoardHeader/>
+      <View>
+        <Button
+          title="Add Crops"
+          onPress={() => props.navigation.navigate('DashBoard - Add Crops')}
+        />
+      </View>
+      <DashBoardTracker/>
     </View>
   )
 }

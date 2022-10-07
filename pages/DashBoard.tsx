498116c45bb6224afefdddba035a2c87861c1988
Yan Wong <kelvinwong26546@gmail.com>
2022-10-07 17:06:38 +0800

    implemented stack nav

--- pages/DashBoard.tsx
+++ pages/DashBoard.tsx
@@ -1,13 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import DashBoardHome from './DashBoardHome'
+import { createStackNavigator } from '@react-navigation/stack';
+import CropsForm from './CropsForm';
+
+const Stack = createStackNavigator();
 
 const DashBoard = () => {
   return (
-    <View>
-        <View>
-            
-        </View>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Calender">
+        {props => <DashBoardHome navigation={props.navigation}/>}
+      </Stack.Screen>
+      <Stack.Screen name="Form" component={(props)=><CropsForm/>} />
+    </Stack.Navigator>
   )
 }
 

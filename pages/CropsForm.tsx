9902e6815d737fa19f6d05dcbcc589bcb078b2e5
Yan Wong <kelvinwong26546@gmail.com>
2022-10-07 17:07:32 +0800

    created Form page for dashboard

---
+++ pages/CropsForm.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CropsForm = () => {
+  return (
+    <View>
+      <Text>CropsForm</Text>
+    </View>
+  )
+}
+
+export default CropsForm

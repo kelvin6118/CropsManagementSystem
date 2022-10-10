c89a1f3cb0c55e1a8a4d447f6f9f1ec25077f1bd
Yan Wong <kelvinwong26546@gmail.com>
2022-10-10 22:41:10 +0800

    add calender calculation

--- screens/DashBoard/DashBoard.tsx
+++ screens/DashBoard/DashBoard.tsx
@@ -7,6 +7,32 @@ import CropsForm from './CropsForm';
 const Stack = createStackNavigator();
 
 const DashBoard = () => {
+  const weekdays:string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const dt = new Date();
+
+  const day = dt.getDate();
+  const month = dt.getMonth();
+  const year = dt.getFullYear();
+
+  const firstDayOfMonth = new Date(year, month, 1);
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  const dateString = firstDayOfMonth.toLocaleDateString('en-GB', {
+    weekday: "long",
+    year: "numeric",
+    month: 'numeric',
+    day: 'numeric',
+  });
+
+  const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+  let date:number = 9 - paddingDays;
+
+  let mon = dt.toLocaleDateString('en-GB', { month: 'numeric'});
+
+  let daySquareDate = `${date}/${mon}/${year}`;
+
+  console.log(daySquareDate)
+  
   return (
     <Stack.Navigator>
       <Stack.Screen name="DashBoard - Home">
